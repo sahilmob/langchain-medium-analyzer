d3c1bf6e1102c3cbf07d3a0cbf53d8a141c0d072
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-03-01 16:24:58 +0300

    embeddings wip

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-if __name__ == "main":
+import os
+from dotenv import load_dotenv
+from langchain.document_loaders.text import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+
+load_dotenv()
+
+
+if __name__ == "__main__":
     print("Hello World")
+    loader = TextLoader(
+        "/Users/sahilmobaidin/Desktop/myprojects/vectordb-db-intro/medium-blogs/mediumblog.txt"
+    )
+    document = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text = text_splitter.split_documents(document)
+    print(len(text))
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
